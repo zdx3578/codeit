4b94b24164aa83e2bc7701b74c8d7aaa464341cb
AGI <zdx3578@gmail.com>
2024-10-05 11:42:39 +0800

    Update __init__.py

--- codeit/__init__.py
+++ codeit/__init__.py
@@ -1,5 +1,4 @@
 # Copyright (c) 2024 Qualcomm Technologies, Inc.
 # All Rights Reserved.
 
-location_init_file = os.path.dirname(os.path.realpath(__file__))
-PROJECT_FOLDER_PATH = location_init_file.split("codeit")[0]
+
